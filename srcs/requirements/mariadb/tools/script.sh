63488a6451baa75b4373d34df04a63bbbe906feb
Ali Reza Shirzad <ashirzad@2-C-2.42heilbronn.de>
2025-01-18 15:49:57 +0100

    chnage the pass

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,6 +3,7 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# Bind MySQL to all IP addresses
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Start the MySQL service
@@ -14,6 +15,7 @@ until mysqladmin ping &>/dev/null; do
   sleep 2
 done
 
+# Set the root password and create the database/user
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
@@ -21,7 +23,6 @@ GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
-# Stop the service to allow the connection from any IP address from the network
-kill $(cat /run/mysqld/mysqld.pid)
 
-mysqld_safe
+# Restart MySQL to apply changes
+service mysql restart
